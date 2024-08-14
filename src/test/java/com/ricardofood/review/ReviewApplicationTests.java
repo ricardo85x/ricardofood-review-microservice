24669fa8f5a2c9fcc7aea6e0df393569c5dd2ee0
ricardo <ricardo85x@gmail.com>
2024-08-14 17:29:15 -0300

    feat: prepare for deployment

--- src/test/java/com/ricardofood/review/ReviewApplicationTests.java
+++ src/test/java/com/ricardofood/review/ReviewApplicationTests.java
@@ -1,10 +1,14 @@
 package com.ricardofood.review;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ReviewApplicationTests {
+@WebMvcTest(ReviewApplication.class)
+@AutoConfigureMockMvc
+@DisplayName("Review Application Test")class ReviewApplicationTests {
 
 	@Test
 	void contextLoads() {
